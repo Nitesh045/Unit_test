a931c06fd16bed9788ce4c901cad79625e00c971
Nitesh Yadav <niteshyadav604@gmail.com>
2024-05-14 12:07:13 +0530

    add some

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { Counter } from './Component/Counter';
 import { UseRef } from './Component/UseRef';
-
+import { createContext } from 'react';
+import { MyComponent } from './Component/Consumer';
+import { MyProvider } from './Component/UseContext';
+ 
 function App() {
+  
   return (
-    <div className="App">
-      <Counter/>
-      <UseRef/>
-    </div>
+    <MyProvider>
+    <MyComponent />
+  </MyProvider>
   );
 }
 
